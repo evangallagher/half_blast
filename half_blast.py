5709cc46c912edb954c45aca79d91b7d2548068d
Shawn Laursen <shla9937@colorado.edu>
2019-08-05 10:24:19 -0600

    Add files via upload
    Better version. this will take a multifasta file instead of some oddly formatted list like the older one.

--- half_blast.py
+++ half_blast.py
@@ -1,27 +1,43 @@
-#need to load blast, can do this using 
-#need to load python 3
-
-#module load ncbi-blast
-#module load python/3.6.3
-
-#then if you have the script saved as a .py
-#python3.6 half_blast.py
-
 import subprocess
-import csv
-import time
 
-with open('test.csv', 'r') as csvfile:
-    data = list(csv.reader(csvfile))
+openthisfile = 'halo_sal_genome.fa'
+data = []
+sequence = None
+seq_len = 0
+
+with open(openthisfile, 'r') as fastafile:
+        for line in fastafile:
+            if '>' in line:
+                if sequence is not None:
+                    seq_len = len(sequence)
+                    protein.append(sequence)
+                    protein.append(seq_len)
+                    sequence = None
+                    data.append(protein)
+                protein = []
+                protein.append(line.rstrip('\n'),)
+            elif (len(line) != 0) and (sequence is not None):
+                sequence += (line.rstrip('\n'))
+            elif (len(line) != 0) and (sequence is None):
+                sequence = (line.rstrip('\n'))
+seq_len = len(sequence)
+protein.append(sequence)
+protein.append(seq_len)
+data.append(protein)
+
+with open('half_blast.out', 'a') as outfile:
+    outfile.write('Half Blast of: '+openthisfile+'\n'+'query acc.ver, subject acc.ver, % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score')
+    outfile.close()
+    fastafile.close()
 
 for gene in data:
     with open('database_file', 'w') as database_file, open('query_file', 'w') as query_file, open('half_blast.out', 'a') as outfile:
-        midpoint = int((int(gene[3]))/2)
-        first_half = gene[2][:(midpoint+1)]
-        second_half = gene[2][(int(gene[3])-midpoint):]
-        database_file.write('>'+gene[0]+'\n'+first_half)
+        midpoint = int((int(gene[2]))/2)
+        first_half = gene[1][:(midpoint+1)]
+        second_half = gene[1][(int(gene[2])-midpoint):]
+        database_file.write(gene[0]+'\n'+first_half)
         database_file.close()
-        query_file.write('>'+gene[0]+'\n'+second_half)
+        query_file.write(gene[0]+'\n'+second_half)
         query_file.close()
         subprocess.call(['makeblastdb', '-in', 'database_file', '-parse_seqids', '-title', '"half_blast"', '-dbtype', 'prot'])
         from subprocess import PIPE, run
@@ -30,3 +46,6 @@ for gene in data:
         result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
         outfile.write(result.stdout)
         outfile.close()
+
+
+
